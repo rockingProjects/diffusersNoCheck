c69e554493243c8d4574636599a860eb9c727eea
udo <udo.bischof@kickbuzz.com>
2022-09-18 14:50:19 +0200

    Disabled nsfw check

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
@@ -303,7 +303,8 @@ class StableDiffusionInpaintPipeline(DiffusionPipeline):
 
         # run safety checker
         safety_cheker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(self.device)
-        image, has_nsfw_concept = self.safety_checker(images=image, clip_input=safety_cheker_input.pixel_values)
+        #image, has_nsfw_concept = self.safety_checker(images=image, clip_input=safety_cheker_input.pixel_values)
+        has_nsfw_concept = False
 
         if output_type == "pil":
             image = self.numpy_to_pil(image)
